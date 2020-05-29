2073eaf9046ae242afc768bd987fc8f3c3ce5eb5
Eric Anderson <eanders@microsoft.com>
2020-05-29 15:56:24 -0700

    vector math

--- vector3.ts
+++ vector3.ts
@@ -10,5 +10,28 @@ namespace threed {
             this.y = y;
             this.z = z;
         }
+
+        public static Multiply(k: number, vec: Vector3) {
+            return new Vector3(k * vec.x, k * vec.y, k * vec.z);
+        }
+
+        public static Dot(v1: Vector3, v2: Vector3) {
+            return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+        }
+
+        public static Cross(v1: Vector3, v2: Vector3) {
+            return new Vector3(
+                v1.y * v2.z - v1.z * v2.y,
+                v1.z * v2.x - v1.x * v2.z,
+                v1.x * v2.y - v1.y * v2.x);
+        }
+
+        public static Add(v1: Vector3, v2: Vector3) {
+            return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
+        public static Magnitude(vec: Vector3) {
+            return Math.sqrt(Vector3.Dot(vec, vec));
+        }
     }
 }
