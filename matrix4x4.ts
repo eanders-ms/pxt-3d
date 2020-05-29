f469eda0de69dba9d2bb2d5b6c13305bb85d8ed0
Eric Anderson <eanders@microsoft.com>
2020-05-29 16:27:54 -0700

    rotation math

--- matrix4x4.ts
+++ matrix4x4.ts
@@ -6,7 +6,7 @@ namespace threed {
             this.data = data;
         }
 
-        public static MakeIdentity() {
+        public static Identity() {
             return new Matrix4x4([
                 [1, 0, 0, 0],
                 [0, 1, 0, 0],
@@ -14,11 +14,43 @@ namespace threed {
                 [0, 0, 0, 1]]);
         }
 
-        public static MakeRotationMatrix(angles: Vector3) {
+        public static RotationMatrixFromEulerAngles(angles: Vector3) {
+            const α = angles.x * Math.PI / 180.0;
+            const β = angles.y * Math.PI / 180.0;
+            const γ = angles.z * Math.PI / 180.0;
 
+            const sα = Math.sin(α);
+            const cα = Math.cos(α);
+            const sβ = Math.sin(β);
+            const cβ = Math.cos(β);
+            const sγ = Math.sin(γ);
+            const cγ = Math.cos(γ);
+
+            return new Matrix4x4([
+                [cα * cβ, cα * sβ * sγ - sα * cγ, cα * sβ * cγ + sα * sγ, 0],
+                [sα * cβ, sα * sβ * sγ + cα * cγ, sα * sβ * cγ - cα * sγ, 0],
+                [-sβ, cβ * sγ, cβ * cγ, 0],
+                [0, 0, 0, 1]
+            ]);
         }
 
-        public static MakeTranslationMatrix(translation: Vector3) {
+        public static RotationMatrixFromAxisAngle(axis: Vector3, angle: number) {
+            const θ = angle * Math.PI / 180.0;
+            const s = Math.sin(θ);
+            const c = Math.cos(θ);
+            const x = axis.x;
+            const y = axis.y;
+            const z = axis.z;
+
+            return new Matrix4x4([
+                [c + x * x * (1 - c), x * y * (1 - c) - x * s, x * z * (1 - c) + y * s, 0],
+                [y * x * (1 - c) + x * s, c + y * y * (1 - c), y * z * (1 - c) - x * s, 0],
+                [z * x * (1 - c) - y * s, z * y * (1 - c) + x * s, c + z * z * (1 - c), 0],
+                [0, 0, 0, 1]
+            ]);
+        }
+
+        public static TranslationMatrix(translation: Vector3) {
             return new Matrix4x4([
                 [1, 0, 0, translation.x],
                 [0, 1, 0, translation.y],
@@ -26,7 +58,7 @@ namespace threed {
                 [0, 0, 0, 1]]);
         }
 
-        public static MakeScalingMatrix(scale: number) {
+        public static ScalingMatrix(scale: number) {
             return new Matrix4x4([
                 [scale, 0, 0, 0],
                 [0, scale, 0, 0],
@@ -58,10 +90,10 @@ namespace threed {
                 }
             }
 
-            return result;            
+            return result;
         }
 
-        public MakeTransposed(mat: Matrix4x4) {
+        public Transposed(mat: Matrix4x4) {
             const result = new Matrix4x4([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]);
 
             for (let i = 0; i < 4; i++) {
@@ -69,8 +101,8 @@ namespace threed {
                     result.data[i][j] = mat.data[j][i];
                 }
             }
-            
-            return result;            
+
+            return result;
         }
     }
 }
