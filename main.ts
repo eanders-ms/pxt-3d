a071612b1b4ed04aa17236a19f5b3572e12c5481
Eric Anderson <eanders@microsoft.com>
2020-05-29 22:15:40 -0700

    cube

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-let engine = new threed.Engine();
+const engine = new threed.Engine();
+const cubeModel = threed.Model.Cube(threed.Colors.Red);
+const cubeInst = new threed.Instance(cubeModel, new threed.Vector3(0, 0, -10), new threed.Vector3(45, 45, 0), 1);
+engine.instances.push(cubeInst);
 game.onUpdateInterval(10, function () {
     engine.draw();
 })
