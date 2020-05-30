ba98b685fc32c344448b97025f0dd2d61b08e91a
Eric Anderson <eanders@microsoft.com>
2020-05-29 23:14:53 -0700

    remove models

--- engine.ts
+++ engine.ts
@@ -1,20 +1,16 @@
 
 namespace threed {
     export class Engine {
-        private _sequence = 0;
-        private _models: { [name: string]: Model };
         private _instances: Instance[];
         private _camera: Camera;
         private _light: Light;
         private _renderer: Renderer;
 
-        public get models() { return this._models; }
         public get instances() { return this._instances; }
         public get camera() { return this._camera; }
         public get light() { return this._light; }
 
         constructor() {
-            this._models = {};
             this._instances = [];
             this._camera = new Camera(new Vector3(0, 0, 0), new Vector3(0, 0, 0));
             this._light = new Light(new Vector3(0, 1, 0));
@@ -24,6 +20,7 @@ namespace threed {
         }
 
         public step(dt: number) {
+            // Handle input
             // Update animations
             // Update physics
             // Check kill plane
